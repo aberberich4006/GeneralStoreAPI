066ca4bce3a7976f542bfa6d2a9b65226c57bdf0
Aaron Berberich <aberberich4006@gmail.com>
2020-08-27 12:02:00 -0400

    Defined Controllers, need to build crud

--- GeneralStoreAPI/Models/Transaction.cs
+++ GeneralStoreAPI/Models/Transaction.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +9,19 @@ namespace GeneralStoreAPI.Models
 {
     public class Transaction
     {
+        [Key]
         public int ID { get; set; }
+        [Required]
+        [ForeignKey("Customer")]
         public int CustomerId { get; set; }
         public virtual Customer Customer { get; set; }
-        public int ProductId { get; set; }
+        [Required]
+        [ForeignKey("Customer")]
+        public string ProductSKU { get; set; }
         public virtual Product Product { get; set; }
+        [Required]
+        public int ItemCount { get; set; }
+        [Required]
         public DateTime DateOfTransaction { get; set; }
 
     }
